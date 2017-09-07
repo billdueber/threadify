9e15b49d3a3c07fb89441338b77b445f68f794c2
Bill Dueber <bill@dueber.com>
2017-09-07 16:45:16 -0400

    Added tests vs non-threaded code

--- spec/concurrency_spec.rb
+++ spec/concurrency_spec.rb
@@ -2,6 +2,20 @@ require 'benchmark'
 RSpec.describe "Threadify concurrancy" do
 
   include_context "shared stuff" # in file spec/shared_stuff.rb
+
+  it "runs about the same with one thread" do
+    onethread = Threadify::Enumerator.new(enum, threads: 1)
+    nonthreaded = Benchmark.realtime { enum.each{|x| sleep 0.05}}
+    threaded = Benchmark.realtime { onethread.each{|x| sleep 0.05}}
+    expect(nonthreaded).to be_within(nonthreaded * 0.10).of(threaded)
+  end
+
+  it "runs faster with two threads" do
+    twothreads = Threadify::Enumerator.new(enum, threads: 2)
+    nonthreaded = Benchmark.realtime { enum.each{|x| sleep 0.05}}
+    threaded = Benchmark.realtime { twothreads.each{|x| sleep 0.05}}
+    expect(nonthreaded).to be_within(nonthreaded * 0.10).of(2 * threaded)
+  end
   
   it "runs N x-second sleeps in about x seconds where N < #threads" do
     fiver    = Threadify::Enumerator.new(1..(threads - 1))
