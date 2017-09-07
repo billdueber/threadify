568311a0a217373dbbcdbaf1dbbd082c9790123e
Bill Dueber <bill@dueber.com>
2017-09-07 16:38:07 -0400

    Added spec for short queue

--- spec/concurrency_spec.rb
+++ spec/concurrency_spec.rb
@@ -14,4 +14,12 @@ RSpec.describe "Threadify concurrancy" do
     realtime = Benchmark.realtime {longer.each {|x| sleep(wait_time)}}
     expect(realtime).to be_within(0.02).of(3 * wait_time)
   end
+
+  it "takes longer if the queue is short" do
+    shortqueue = Threadify::Enumerator.new(enum, max_queue: 4)
+    normal_realtime = Benchmark.realtime {te.each {|x| sleep(wait_time)}}
+    shortqueue_realtime = Benchmark.realtime {shortqueue.each {|x| sleep(wait_time)}}
+    expect(shortqueue_realtime.to_f - normal_realtime).to be_within(0.05).of(wait_time)
+
+  end
 end
