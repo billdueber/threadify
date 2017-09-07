71a28dea8e10ac03ad5c700bca41b09fc35ca950
Bill Dueber <bill@dueber.com>
2017-09-07 13:40:09 -0400

    Share let statements bewteen specs

---
+++ spec/shared_stuff.rb
@@ -0,0 +1,19 @@
+RSpec.configure do |rspec|
+  rspec.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+RSpec.shared_context "shared stuff", :shared_context => :metadata do
+
+  let(:enum) {5..15}
+  let(:last_value) {15}
+  let(:slow_value) {8}
+  let(:wait_time) { 0.2 }
+  let(:threads) { 5 }
+  let(:te) {Threadify::Enumerator.new(enum)}
+
+
+end
+
+RSpec.configure do |rspec|
+  rspec.include_context "shared stuff", :include_shared => true
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,9 @@ require 'bundler/setup'
 require 'threadify'
 
 RSpec.configure do |config|
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -70,7 +73,7 @@ RSpec.configure do |config|
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
   # particularly slow.
-  config.profile_examples = 10
+  # config.profile_examples = 10
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
